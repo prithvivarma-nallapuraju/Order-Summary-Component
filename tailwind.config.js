fd87f747e78c20881565e4cfe94c0b498a842a49
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2023-01-16 11:00:13 +0530

    completed the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "376px",
+      xl: "1440px",
+    },
+    extend: {
+      backgroundImage: {
+        desktopImage: "url('/images/pattern-background-desktop.svg')",
+        mobileImage: "url('/images/pattern-background-mobile.svg')",
+      },
+      colors: {
+        paleBlue: "hsl(225, 100%, 94%)",
+        brightBlue: "hsl(245, 75%, 52%)",
+        veryPaleBlue: "hsl(225, 100%, 98%)",
+        desaturatedBlue: "hsl(224, 23%, 55%)",
+        darkBlue: "hsl(223, 47%, 23%)",
+      },
+      fontFamily: {
+        redHat: ["Red Hat Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
